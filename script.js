0fb8bd9d76207930cf009f4c1795ffc52d432011
Rafael Nelson <rafaelson1337@gmail.com>
2022-04-15 22:46:49 -0300

    added modal window with a form to add books

--- script.js
+++ script.js
@@ -12,17 +12,44 @@ const o1 = new Book("Lorem and Ipsum", "Nelson", 345, "read");
 const o2 = new Book("Something", "Nothing", 636, "not read yet");
 const o3 = new Book("dasopdaso", "dsfjisjjkd", 12, "read");
 
-let myLibrary = [o1, o2, o3];
-
+let myLibrary = [];
 const container = document.querySelector(".container");
+const form = document.querySelector(".form");
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
+const bookPages = document.querySelector("#book-pages");
+const bookStatus = document.querySelector("#book-status");
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", addBookToLibrary);
 
-const startButton = document.querySelector(".start");
-startButton.addEventListener("click", () => {
-  startButton.remove();
+document.addEventListener("DOMContentLoaded", showInDOM, false);
+document.querySelector(".add-button").addEventListener("click", openForm);
+
+// const startButton = document.querySelector(".start");
+// startButton.addEventListener("click", () => {
+//   startButton.remove();
+//   showInDOM();
+// });
+
+function addBookToLibrary() {
+  let status;
+  if (bookStatus.checked) {
+    status = "read";
+  } else {
+    status = "not read yet";
+  }
+  myLibrary[myLibrary.length] = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    status
+  );
   showInDOM();
-});
+}
 
-function addBookToLibrary() {}
+function openForm() {
+  form.classList.toggle("show-form");
+}
 
 function showInDOM() {
   for (i = 0; i < myLibrary.length; i++) {
